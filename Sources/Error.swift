71946c598532f3a02b5a7eccba5084fae65663c8
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-02-11 12:10:21 +0300

    Simplify SystemError

--- Sources/Error.swift
+++ Sources/Error.swift
@@ -1,8 +1,5 @@
 public struct SystemError: Error, CustomStringConvertible {
     public init(){}
-    private let message = String(cString: strerror(errno))
     public let number = errno
-    public var description: String {
-        return "\(number): \(message)"
-    }
+    public let description = String(cString: strerror(errno))
 }
