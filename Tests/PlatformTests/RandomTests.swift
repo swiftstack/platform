59c9e4c50c7c07e32ac991f8730bdb8366e764b4
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-04-22 22:52:40 +0300

    [Tests] Add shims

--- Tests/PlatformTests/RandomTests.swift
+++ Tests/PlatformTests/RandomTests.swift
@@ -1,17 +1,16 @@
-import XCTest
 @testable import Platform
 
 class RandomTests: XCTestCase {
     func testRandom() {
         let first = arc4random()
         let second = arc4random()
-        XCTAssertNotEqual(first, second)
+        assertNotEqual(first, second)
     }
 
     func testRandomUniform() {
         for _ in 0..<1_000 {
-            XCTAssert(arc4random_uniform(12) >= 0)
-            XCTAssert(arc4random_uniform(12) < 12)
+            assert(arc4random_uniform(12) >= 0)
+            assert(arc4random_uniform(12) < 12)
         }
     }
 
@@ -20,6 +19,6 @@ class RandomTests: XCTestCase {
         var second = [UInt8](repeating: 0, count: 10)
         arc4random_buf(&first, first.count)
         arc4random_buf(&second, second.count)
-        XCTAssertNotEqual(first, second)
+        assertNotEqual(first, second)
     }
 }
