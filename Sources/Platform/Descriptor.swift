2a368e8443a368e58ddcc196faf11efbd4d3a1cb
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-09-14 23:29:25 +0300

    Change Descriptor from typealias to struct

--- Sources/Platform/Descriptor.swift
+++ Sources/Platform/Descriptor.swift
@@ -1,13 +1,20 @@
-public typealias Descriptor = Int32
+public struct Descriptor: RawRepresentable {
+    public let rawValue: Int32
 
-extension Descriptor {
+    public init?(rawValue: Int32) {
+        guard rawValue > 0 else {
+            return nil
+        }
+        self.rawValue = rawValue
+    }
+
     public var flags: Int32 {
-        get { return fcntl(self, F_GETFD, 0) }
-        set { _ = fcntl(self, F_SETFD, newValue) }
+        get { return fcntl(rawValue, F_GETFD, 0) }
+        set { _ = fcntl(rawValue, F_SETFD, newValue) }
     }
 
     public var status: Int32 {
-        get { return fcntl(self, F_GETFL, 0) }
-        set { _ = fcntl(self, F_SETFL, newValue) }
+        get { return fcntl(rawValue, F_GETFL, 0) }
+        set { _ = fcntl(rawValue, F_SETFL, newValue) }
     }
 }
