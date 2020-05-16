512acca4876671c3d42a0f4fa2c49e273cb71e1b
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-05-16 20:37:58 +0300

    Improve SystemError

--- Sources/Platform/Error.swift
+++ Sources/Platform/Error.swift
@@ -1,5 +1,11 @@
-public struct SystemError: Error, CustomStringConvertible {
-    public init(){}
-    public let number = errno
-    public let description = String(cString: strerror(errno))
+public struct SystemError: Error, Equatable {
+    public let number: Int
+
+    public init(number: Int = .init(errno)) {
+        self.number = number
+    }
+}
+
+extension SystemError: CustomStringConvertible {
+    public var description: String { .init(cString: strerror(errno)) }
 }
