7311a6f920a21bfd8706c421a33508585b6602d2
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-05-11 01:54:19 +0300

    Revert "[Tests] Workaround reasync compiler crash"
    The bug has been fixed

--- Tests/Platform/System/main.swift
+++ Tests/Platform/System/main.swift
@@ -2,19 +2,19 @@ import Test
 import Platform
 
 test.case("Int") {
-    await expect(throws: SystemError()) {
+    expect(throws: SystemError()) {
         try system { -1 }
     }
 }
 
 test.case("OpaquePointer") {
-    await expect(throws: SystemError()) {
+    expect(throws: SystemError()) {
         try system { nil as OpaquePointer? }
     }
 }
 
 test.case("GenericPointer") {
-    await expect(throws: SystemError()) {
+    expect(throws: SystemError()) {
         try system { nil as UnsafeMutablePointer<Int>? }
     }
 }
